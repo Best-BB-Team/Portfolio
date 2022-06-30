c458e2a72f26effaff0d515d85d7f0cfd5c9aaca
LegjobbCenter <kalmanbarnabas4@gamil.com>
2022-06-30 23:32:29 +0200

    msin

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 //set port
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
