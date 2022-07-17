17f6c6b1ece482c93578530754096f108347f362
Preisler25 <preisler.andras@gmail.com>
2022-07-17 23:08:34 +0200

    main

--- public/2048/app.js
+++ public/2048/app.js
@@ -1,6 +1,10 @@
 const main = () =>{
+     gameStart();
+}
+
+const gameStart = () =>{
   genrandom();
-  genrandom();   
+  genrandom();
 }
 
 const randomnum = () =>{
@@ -14,79 +18,111 @@ const randomnum = () =>{
 }
 
 const genrandom = () =>{
-  row = Math.floor(Math.random()*4)+1;
-  col = Math.floor(Math.random()*4)+1;
-  id = `col${col}row${row}`;
-  console.log(id);
-  if(document.getElementById(id).innerHTML == ""){
-    document.getElementById(id).innerHTML = randomnum();
-  }
+  isOk = false;
+  do {
+    row = Math.floor(Math.random()*4)+1;
+    col = Math.floor(Math.random()*4)+1;
+    id = `col${col}row${row}`;
+    if(document.getElementById(id).innerHTML == ""){
+      document.getElementById(id).innerHTML = randomnum();
+      isOk = true;
+    }
+  } while (!isOk);
 }
 
+
+//movement
+
+
+
 const moveUp = () =>{
-  all = document.querySelectorAll('.block');
-  all.forEach(e => {
-    if(e.innerHTML != ""){
-    row = parseInt(e.id.slice(7, 8));
-    col = parseInt(e.id.slice(3, 4));
-    if(row != 1){
-      upperRow = row-1
-      upperId =  `col${col}row${upperRow}`;
-      console.log(upperId);
-      myNum = e.innerHTML;
-      condNum = document.getElementById(upperId).innerHTML;
-      if (condNum == myNum || condNum == "") {
-        document.getElementById(upperId).innerHTML += myNum;
-        e.innerHTML = ""
+  for (let i = 0; i < 4; i++) {
+    all = document.querySelectorAll('.block');
+    all.forEach(e => {
+      if(e.innerHTML != ""){
+      row = parseInt(e.id.slice(7, 8));
+      col = parseInt(e.id.slice(3, 4));
+      if(row != 1){
+        upperRow = row-1
+        upperId =  `col${col}row${upperRow}`;
+
+        myNum = e.innerHTML;
+        condNum = document.getElementById(upperId).innerHTML;
+        if (condNum == myNum || condNum == "") {
+          if(condNum == myNum){
+            document.getElementById(upperId).innerHTML = parseInt(condNum) + parseInt(myNum)
+            e.innerHTML = ""
+          }
+          else{
+            document.getElementById(upperId).innerHTML += myNum;
+            e.innerHTML = ""
+          }
+        }
       }
     }
+  });   
   }
-});
-    
-}
+  }
+
 const moveDown = () =>{
+  for (let i = 0; i < 4; i++) {
   all = document.querySelectorAll('.block');
   all.forEach(e => {
     if(e.innerHTML != ""){
     row = parseInt(e.id.slice(7, 8));
     col = parseInt(e.id.slice(3, 4));
     if(row != 4){
-      console.log(row);
       lowerRow = row+1
       lowerId =  `col${col}row${lowerRow}`;
-      console.log(lowerId);
       myNum = e.innerHTML;
       condNum = document.getElementById(lowerId).innerHTML;
       if (condNum == myNum || condNum == "") {
+        if(condNum == myNum){
+          document.getElementById(lowerId).innerHTML = parseInt(condNum) + parseInt(myNum)
+          e.innerHTML = ""
+        }
+        else{
         document.getElementById(lowerId).innerHTML += myNum;
         e.innerHTML = ""
+        }
       }
     }
   }
 });
 }
+}
+
 const moveLeft = () =>{
-  all = document.querySelectorAll('.block');
-  all.forEach(e => {
-    if(e.innerHTML != ""){
-    row = parseInt(e.id.slice(7, 8));
-    col = parseInt(e.id.slice(3, 4));
-    if(col != 1){
-      console.log(col);
-      lefterCol = col-1
-      lefterId =  `col${lefterCol}row${row}`;
-      console.log(lefterId);
-      myNum = e.innerHTML;
-      condNum = document.getElementById(lefterId).innerHTML;
-      if (condNum == myNum || condNum == "") {
-        document.getElementById(lefterId).innerHTML += myNum;
-        e.innerHTML = ""
+  for (let i = 0; i < 4; i++) {
+    all = document.querySelectorAll('.block');
+    all.forEach(e => {
+      if(e.innerHTML != ""){
+      row = parseInt(e.id.slice(7, 8));
+      col = parseInt(e.id.slice(3, 4));
+      if(col != 1){
+        lefterCol = col-1
+        lefterId =  `col${lefterCol}row${row}`;
+        myNum = e.innerHTML;
+        condNum = document.getElementById(lefterId).innerHTML;
+        if (condNum == myNum || condNum == "") {
+          if(condNum == myNum){
+            document.getElementById(lefterId).innerHTML = parseInt(condNum) + parseInt(myNum)
+            e.innerHTML = ""
+          }
+          else{
+          document.getElementById(lefterId).innerHTML += myNum;
+          e.innerHTML = ""
+        }
+      }
       }
     }
-  }
-});
+  });
 }
+}
+  
 const moveRight = () =>{
+  for (let i = 0; i < 4; i++) {
+
   all = document.querySelectorAll('.block');
   all.forEach(e => {
     if(e.innerHTML != ""){
@@ -95,31 +131,42 @@ const moveRight = () =>{
     if(col != 4){
       righterCol = col+1
       righterId =  `col${righterCol}row${row}`;
-      console.log(righterId);
       myNum = e.innerHTML;
       condNum = document.getElementById(righterId).innerHTML;
       if (condNum == myNum || condNum == "") {
+        if(condNum == myNum){
+          document.getElementById(righterId).innerHTML = parseInt(condNum) + parseInt(myNum)
+          e.innerHTML = ""
+        }
+        else{
         document.getElementById(righterId).innerHTML += myNum;
         e.innerHTML = ""
+        }
       }
     }
   }
 });
 }
+}
+//checking movement
 
 const checkArrow = (e) =>{
   switch (e) {
     case "ArrowUp":
-      moveUp()
+      moveUp();
+      genrandom();
       break;
     case "ArrowDown":
-      moveDown()
+      moveDown();
+      genrandom();
       break;
     case "ArrowLeft":
-      moveLeft()
+      moveLeft();
+      genrandom();
       break;
     case "ArrowRight":
-      moveRight()
+      moveRight();
+      genrandom();
       break;
     default:
       break;
@@ -128,8 +175,6 @@ const checkArrow = (e) =>{
 
 document.addEventListener('keydown', (event) => {
     var name = event.key;
-    var code = event.code;
-    // Alert the key name and key code on keydown
     checkArrow(name);
   }, false);
 
